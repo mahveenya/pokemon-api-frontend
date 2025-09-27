5ca65779b8f674fb0eb8e3950b9161ec42612234
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2025-09-27 23:36:25 +0300

    feat: add vitest, rtl, msw

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -5,4 +6,27 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   base: '/react/',
   plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'src/setupTests.ts',
+    coverage: {
+      all: true,
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.d.ts',
+        'src/**/types/**',
+        'src/**/utils/**',
+        'src/**/mocks/**',
+        'src/setupTests.ts',
+      ],
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+    },
+  },
 });
